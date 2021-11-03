0eb85cb3415e5eaa3c4ed4bd2c6a422674f86ae4
ShedrackGodson <hedrickgodson23@gmail.com>
2021-11-03 17:52:41 +0300

    Predicates

--- src/main/java/functionalinterface/_Predicate.java
+++ src/main/java/functionalinterface/_Predicate.java
@@ -1,2 +1,19 @@
-package functionalinterface;public class _Predicate {
+package functionalinterface;
+
+import java.util.function.Predicate;
+
+public class _Predicate {
+    public static void main(String[] args) {
+        System.out.println(isPhoneNumberValid("0767546734"));
+        System.out.println(isPhoneNumberValid("0667546734"));
+        System.out.println(phoneNumberPredicate.test("0775450734"));
+    }
+
+    // Normal Function Validation
+    static boolean isPhoneNumberValid(String phoneNumber) {
+        return phoneNumber.startsWith("07") && phoneNumber.length() == 10;
+    }
+
+    // Predicate Function Validation
+    static Predicate<String> phoneNumberPredicate = phoneNumber -> phoneNumber.startsWith("07") && phoneNumber.length() == 10;
 }
